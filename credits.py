7791d795fd827e8ae85f0862bc0c090df28c5988
kingismail <68245583+kingismail@users.noreply.github.com>
2022-06-17 19:04:26 +0200

    Create credits.py

---
+++ credits.py
@@ -0,0 +1,43 @@
+
+
+
+
+def credits():
+	print("""
+
+  ____      _               ____
+ / ___|   _| |__   ___ _ __/ ___|  ___  ___
+| |  | | | | '_ \ / _ \ '__\___ \ / _ \/ __|
+| |__| |_| | |_) |  __/ |   ___) |  __/ (__
+ \____\__, |_.__/ \___|_|  |____/ \___|\___|
+      |___/
+
+established by:ismail
+""")
+
+def more_creds():
+	print("""
+
+THIS WAS CODED WITH PASSION BY ISMAIL
+
+
+
+
+\033[32m[*]\033[31mWHATSAPP —> +27781402245
+\033[32m[*]\033[93mFACEBOOK -> ISMAIL IBTAHIM
+\033[32m[*]\033[34mGITHUB -> KING ISMAIL
+
+
+  SHOUT OUT TO MNCEDISI🔥🔥
+
+  "WE DECLARE VARIABLES NOT WAR"
+
+
+""")
+
+
+def main_creds():
+	credits()
+	more_creds()
+
+
